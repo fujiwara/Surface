3a5e0fe02fd1326b94c27c26235a6d7d9be227c0
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2012-10-31 17:54:50 +0900

    implement and test

--- lib/Surface.pm
+++ lib/Surface.pm
@@ -1,32 +1,129 @@
 package Surface;
+
 use strict;
 use warnings;
-our $VERSION = '0.01';
+use Redis;
+use Mouse;
+use JSON;
+use Log::Minimal;
+use Time::HiRes qw/ time /;
+use POSIX;
+
+our $VERSION = "0.1";
+
+has redis => (
+    is      => "rw",
+    lazy    => 1,
+    default => sub {
+        my $self = shift;
+        my %options = %{ $self->redis_options };
+        $options{reconnect} = 1 unless defined $options{reconnect};
+        Redis->new(%options);
+    },
+);
+
+has redis_options => (
+    is      => "rw",
+    isa     => "HashRef",
+    default => sub { +{} },
+);
+
+has expires => (
+    is      => "rw",
+    isa     => "Int",
+    default => 300,
+);
+
+has namespace => (
+    is      => "rw",
+    isa     => "Str",
+    default => "default",
+);
+
+no Mouse;
+
+sub register {
+    my $self = shift;
+    my $msg  = shift;
+    my $str  = JSON::encode_json($msg);
+    my $now  = time;
+    my $key  = $self->namespace;
+    debugf("register %s => %s", $key, $str);
+
+    $self->redis->set( "LAST_UPDATED:$key", $now );
+    $self->redis->zadd( $key, $now, $str );
+    $self->redis->rpush( "QUEUE:$key", $str );
+}
+
+sub dequeue {
+    my $self = shift;
+    my $key  = $self->namespace;
+    debugf("fetch %s using blpop", $key);
+    my (undef, $str) = $self->redis->blpop("QUEUE:$key", 1);
+    if (defined $str) {
+        return JSON::decode_json($str);
+    }
+    return;
+}
+
+sub last_updated {
+    my $self = shift;
+    my $key  = $self->namespace;
+    my $t = $self->redis->get("LAST_UPDATED:$key");
+    debugf("LAST_UPDATED:$key = %s", $t);
+    $t;
+}
+
+sub is_active {
+    my $self = shift;
+    my $expired = time - $self->expires;
+    my $lu = $self->last_updated;
+    debugf("%s > %s", $lu, $expired);
+    $self->last_updated > $expired;
+}
+
+sub cleanup {
+    my $self = shift;
+    my $key  = $self->namespace;
+
+    my $last_updated = $self->last_updated;
+    unless ($last_updated) {
+        warnf("can't get LAST_UPDATED:%s", $key);
+        return 0;
+    }
+
+    my $n = $self->redis->zremrangebyscore(
+        $key,
+        0,
+        ($last_updated - $self->expires)
+    );
+    debugf(
+        "%d messages expired by cleanup (before %s)",
+        $n, scalar localtime( $last_updated - $self->expires )
+    );
+    $n;
+}
+
+sub retrieve_all {
+    my $self = shift;
+    my $key  = $self->namespace;
+
+    return map { JSON::decode_json($_) }
+        $self->redis->zrange($key, 0, POSIX::ceil(time));
+}
+
+sub enqueue_all {
+    my $self = shift;
+    my $key  = $self->namespace;
+
+    my $n = 0;
+    for my $str ( $self->redis->zrange($key, 0, POSIX::ceil(time)) ) {
+        $self->redis->rpush( "QUEUE:$key", $str );
+        $n++;
+    }
+    $n;
+}
 
 1;
+
 __END__
-
-=head1 NAME
-
-Surface -
-
-=head1 SYNOPSIS
-
-  use Surface;
-
-=head1 DESCRIPTION
-
-Surface is
-
-=head1 AUTHOR
-
-fujiwara E<lt>fujiwara.shunichiro@gmail.comE<gt>
-
-=head1 SEE ALSO
-
-=head1 LICENSE
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=cut
---
+++ t/01_run.t
@@ -0,0 +1,81 @@
+# -*- mode:perl -*-
+use strict;
+use Redis;
+use Test::RedisServer;
+use Test::More;
+use Test::Pretty;
+
+use_ok "Surface";
+
+my $redis_server;
+
+subtest "setup redis server" => sub {
+    eval {
+        $redis_server = Test::RedisServer->new;
+    } or plan skip_all => 'redis-server is required to this test';
+    ok $redis_server;
+};
+
+my $surface;
+for my $namespace (undef, "testing") {
+
+    subtest "new" => sub {
+        $surface = Surface->new(
+            redis_options => { $redis_server->connect_info },
+            expires       => 3,
+        );
+        ok $surface;
+        $surface->namespace($namespace) if defined $namespace;
+
+        is $surface->expires     => 3;
+        is $surface->namespace   => $namespace || "default";
+        is $surface->redis->ping => "PONG";
+    };
+
+    subtest "register" => sub {
+        ok $surface->register({ foo => 1 });
+        ok $surface->register({ bar => 2 });
+        ok $surface->register({ baz => 3 });
+
+        is_deeply [ $surface->retrieve_all ] => [{ foo => 1 }, { bar => 2 }, { baz => 3 }];
+        ok $surface->is_active;
+    };
+
+    subtest "dequeue" => sub {
+        my $msg;
+        ok $msg = $surface->dequeue;
+        is_deeply $msg => { foo => 1 };
+        ok $msg = $surface->dequeue;
+        is_deeply $msg => { bar => 2 };
+        ok $msg = $surface->dequeue;
+        is_deeply $msg => { baz => 3 };
+        ok not $surface->dequeue;
+    };
+
+    sleep $surface->expires;
+
+    subtest "inactive" => sub {
+        ok not $surface->is_active;
+        is $surface->cleanup => 0;
+        is_deeply [ $surface->retrieve_all ] => [{ foo => 1 }, { bar => 2 }, { baz => 3 }];
+
+        $surface->register({ xxx => 3 });
+        $surface->register({ bar => 2 });
+        $surface->register({ foo => 1 });
+        ok $surface->is_active;
+        is $surface->cleanup => 1;
+    };
+
+    subtest "enqueue_all" => sub {
+        ok $surface->enqueue_all;
+        my $msg;
+        ok $msg = $surface->dequeue;
+        is_deeply $msg => { xxx => 3 };
+        ok $msg = $surface->dequeue;
+        is_deeply $msg => { bar => 2 };
+        ok $msg = $surface->dequeue;
+        is_deeply $msg => { foo => 1 };
+    };
+}
+
+done_testing;
